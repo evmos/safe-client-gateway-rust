fb41449b82fbd489428722324da498b57fdd9337
Daniel Russian <danielrussian@gmail.com>
2022-05-30 12:33:43 -0300

    Evmos stg (#1)
    * fix: Update .sh scripts
    
    * fix: .sh format
    
    Co-authored-by: Aliaksei Malyshau <aliaksei.malyshau@altoros.com>

--- .codebuild/docker-build.sh
+++ .codebuild/docker-build.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 
 # Create Dockerfiles for components
-mv .codebuild/nginx.docker .codebuild/config_nginx.docker
+mv .codebuild/nginx.docker .codebuild/gateway_nginx.docker
 
-for file in config_web
-do
+for file in gateway_web; do
   cp Dockerfile .codebuild/$file.docker
 done
 
-
 # Build Docker images
-for file in .codebuild/*.docker
-do
+for file in .codebuild/*.docker; do
   tag=$(basename -- "$file" ".${file##*.}")
   docker build -t $tag -f $file .
 done
-
